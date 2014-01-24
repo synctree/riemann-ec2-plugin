952e4cae46188bd666ed2f7976018c3725d50ac2
Torbjörn Norinder <torbjorn@campanja.com>
2014-01-24 11:58:25 +0100

    Add docstrings to functions

--- src/riemann/plugin/ec2.clj
+++ src/riemann/plugin/ec2.clj
@@ -22,16 +22,27 @@
         (Thread/sleep interval)))))
 
 (defn start-ec2 [opts]
+  "Creates a thread-service which updates the cache used to retrieve information
+  regarding EC2 instance status"
   (let [service (ec2-service opts)]
     (swap! config/next-core core/conj-service service :force)
     service))
 
 (defn get-host-info [host]
+  "Retrieve information regarding one host"
   (first
     (filter
       (comp (partial = (shorthost host)) shorthost :private-dns-name) @cache)))
 
 (defn running-stream [& children]
+  "Provides a stream which will only pass on events from hosts with state \"running\"
+  to children.
+
+  (ec2/running-stream (partial prn \"a running host sent me an event\"))
+
+  Note: data regarding instance status is retrieved from a cache. This cache is updated by
+  a background thread which you must start from your configuration using the start-ec2 function
+  provided by this namespace."
   (fn [event]
     (when-let [host (:host event)]
       (when-let [ec2-info (get-host-info host)]
